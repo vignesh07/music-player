a57f04c14ab2b74da49ab4081b9708fc546f97c9
Vignesh Natarajan <vignesh.natarajan92@gmail.com>
2014-02-17 23:42:47 +0530

    first commit, alpha version

---
+++ player.py
@@ -0,0 +1,49 @@
+import os
+from os import listdir
+import pyglet
+import random
+from pyglet import app
+from pyglet import window
+
+song_list=[]   ##empty list to store the list of songs
+
+for file in os.listdir("."):
+	if ".mp3" in file:
+		song_list.append(file)           ##listdir will list all files and even the subdirectories, so I just 
+										 ##used a workaround to add only the mp3 files
+
+
+player = pyglet.media.Player()           ##player object has the option of queueing
+
+random.shuffle(song_list)
+random.shuffle(song_list)
+random.shuffle(song_list)                ##making the playlist as random as possible
+
+for song in song_list:
+	
+	audio = pyglet.resource.media(song)
+	player.queue(audio)
+
+
+
+player.play()
+pyglet.app.run()
+
+
+
+	
+
+
+
+
+
+	
+
+
+
+
+
+	
+	
+
+	
